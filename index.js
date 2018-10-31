c9a0b50af585140e7a21e040aaae89eb05c420ce
Muhammad <maljuburi89@gmail.com>
2018-10-31 14:04:28 -0700

    added Pug template to render home page

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ const app = express();
 require("./DB/db")();
 require("./middleware/prod")(app);
 
+app.set("view engine", "pug");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/posts", posts);
+
+app.get("/", (req, res) => {
+  const data = {
+    site_title: "POST API",
+    header: "Welcome to this RESTful API"
+  };
+  res.render("home", data);
+});
 
 console.log(config.get("name"));
 console.log(process.env.NODE_ENV);
