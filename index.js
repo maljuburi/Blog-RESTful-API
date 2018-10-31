7caed3422a9766962debf7fc85620ea73579c865
Muhammad <maljuburi89@gmail.com>
2018-10-30 22:55:06 -0700

    added production file to middleware

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const express = require("express");
 const app = express();
 
 require("./DB/db")();
+require("./middleware/prod")(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
